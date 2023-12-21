63131df3f1723ebcfe836aa9a20b445d6a3a19f7
Lucas Meyer <lucas.meyer@microsoft.com>
2023-12-21 12:08:04 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SemanticKernel;
+using Plugins;
+
+var (apiKey, orgId) = Settings.LoadFromFile();
+
+// Create kernel
+var builder = Kernel.CreateBuilder();
+builder.Plugins.AddFromType<Dalle3>();
+var kernel = builder.Build();
+
+// Test the math plugin
+string answer = await kernel.InvokeAsync<string>(
+"Dalle3", "ImageFromPrompt",
+    new() {
+        { "prompt", "cat" }
+    }
+);
+
+Console.Write(answer);
